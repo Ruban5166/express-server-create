50bb9345ef9d3ee31bafb59867d8becd58d5de03
Ruban5166 <49543080+Ruban5166@users.noreply.github.com>
2025-08-29 12:32:53 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-
 import express from "express"
 const app = express();
 const port = 3000;
@@ -6,3 +5,18 @@ const port = 3000;
 app.listen(port,()=>{
 console.log(`the server is started port ${port}`)
 })
+
+
+app.get("/",(req,res)=>{
+    res.send("<h1>well come to home</h1>")
+})
+
+app.get("/AboutUs",(req,res)=>{
+    res.send("<h1>My name is Ruban</h1>")
+})
+
+
+app.get("/contact",(req,res)=>{
+    res.send("<h1>email:sriruban5199@gmail.com ,mob:8714158251</h1>")
+})
+
